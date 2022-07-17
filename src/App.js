a2e4269a40ee0a00e614b4d0f31f1ddc49e22326
Anas Ashraf <anas.ash099@gmail.com>
2022-07-17 23:45:52 +0200

    fixed routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, useParams, Route, UNSAFE_RouteContext, useNavigate, Navigate, Outlet} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, Outlet} from "react-router-dom";
 import ChatBody from './ChatBody';
+
 function App() {
   return (
     <Router> 
